7aeba44cf26b3ee82584ed660d5e2a4c5b2c5952
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-27 09:47:48 +0330

    Set EntinityNotFoundException with 3 constructors

--- DataBase/src/db/exception/EntityNotFoundException.java
+++ DataBase/src/db/exception/EntityNotFoundException.java
@@ -1,4 +1,13 @@
 package db.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends Exception{
+    public EntityNotFoundException(){
+        super("Cannot find entity.");
+    }
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+    public EntityNotFoundException(int id){
+        super("Cannot find entity with id={" + id + "}");
+    }
 }
