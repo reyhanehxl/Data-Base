fb2a0fad4ebae2300e088d30b26aced3a31a5b0a
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-27 09:32:23 +0330

    Add static ArrayList<Entity> for entity storage

--- DataBase/src/db/Database.java
+++ DataBase/src/db/Database.java
@@ -1,4 +1,14 @@
 package db;
 
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
 public class Database {
+    private static final ArrayList<Entity> entities = new ArrayList<>();
+
+    public static void add(Entity e) {
+        e.id = entities.size() + 1;
+        entities.add(e);
+    }
 }
