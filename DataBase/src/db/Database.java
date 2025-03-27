0ecaced14f8ff6a74c04c207d141c5b3d7da532d
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-27 09:33:53 +0330

    Add add(Entity e): auto generate id

--- DataBase/src/db/Database.java
+++ DataBase/src/db/Database.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 
 public class Database {
     private static final ArrayList<Entity> entities = new ArrayList<>();
-
-    public static void add(Entity e) {
+    public static void add(Entity e){
         e.id = entities.size() + 1;
         entities.add(e);
     }
