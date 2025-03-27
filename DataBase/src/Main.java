97f32fee184901333d2bc8d5c794a52e64e3c297
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-27 11:55:57 +0330

    Add Human and Main to test the code

--- DataBase/src/Main.java
+++ DataBase/src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import db.Database;
+import db.exception.EntityNotFoundException;
+import example.Human;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws EntityNotFoundException{
+        Human[] humans = {
+                new Human("Gholi"),
+                new Human("Jamshid"),
+                new Human("Akbar"),
+        };
+
+        System.out.println("#### Test add method ####");
+
+        for (Human h : humans) {
+            System.out.println("Adding " + h.name + " to the database.");
+            Database.add(h);
+        }
+
+        for (Human h : humans) {
+            System.out.println("Id of \"" + h.name + "\" is " + h.id + ".");
+        }
+
+        System.out.println();
+        System.out.println("#### Test get method ####");
+
+        int gholiId = humans[0].id;
+        Human gholi = (Human) Database.get(gholiId);
+
+        System.out.println("successfully got " + gholi.name + " from the database.");
+
+        System.out.println();
+        System.out.println("#### Test update method ####");
+
+        gholi.name = "Gholi Mohammadi";
+        Database.update(gholi);
+
+        Human gholiAgain = (Human) Database.get(gholiId);
+        System.out.println("Updated name: \"" + gholiAgain.name + "\".");
+
+        System.out.println();
+        System.out.println("#### Test delete method ####");
+
+        int jamshidId = humans[1].id;
+        Database.delete(jamshidId);
+
+        try {
+            Human jamshid = (Human) Database.get(jamshidId);
+        } catch (EntityNotFoundException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- DataBase/src/db/Entity.java
+++ DataBase/src/db/Entity.java
@@ -2,4 +2,11 @@ package db;
 
 public abstract class Entity {
     public int id;
+    public int getId() {
+        return id;
+    }
+
+    protected void setId(int id) {
+        this.id = id;
+    }
 }
