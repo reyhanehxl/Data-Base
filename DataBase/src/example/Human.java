576ebcc0755329e42770c32d21dd604b138a874c
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-29 16:15:10 +0330

    feat: add age field to Human class

--- DataBase/src/example/Human.java
+++ DataBase/src/example/Human.java
@@ -4,10 +4,11 @@ import db.Entity;
 
 public class Human extends Entity implements Cloneable{
     public String name;
+    public int age;
 
-    public Human(String name) {
-
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
     @Override
     public Human clone() throws CloneNotSupportedException{
